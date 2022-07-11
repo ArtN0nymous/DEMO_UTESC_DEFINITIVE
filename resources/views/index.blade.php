0aa419d62b4af9645d065badf4846f1260cb4daf
Alexis-dev508 <alexisjesus773@gmail.com>
2022-07-11 10:51:43 -0600

    Subido el footer de ramiro

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,16 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/sidebar.css')}}">
-   
+    <link rel="stylesheet" href="{{asset('css/footer.css')}}">
     <link rel="stylesheet" href="{{asset('bootstrap-icons/font/bootstrap-icons.css')}}">
     <title>Inicio</title>
 </head>
 <body>
      <x-navbar/>
     <div class="container">
+        <footer class="foo">
         
+            <div class="im">
+                <img src="{{asset('img/utesc_logo_movil.png')}}" class="im_image">
+            </div>
+            <div  class="copi">
+                <label for="" >C UTESC. camino al guacimal S/N, al norte da la ciudad, colonia centro, C.P.82400,Escuinapa, Sinaloa, Mexico, Telefono(695) 110 6779</label>
+            </div>
+            <div  class="lin">
+                <ul class="lin_lista">
+                    <li><a class="lin_lista_ul--a" href="">Usuarios</a></li>
+                    <li><a class="lin_lista_ul--a" href="">Solicitudes</a></li>
+                    <li><a class="lin_lista_ul--a" href="">Bodega</a></li>
+                    <li><a class="lin_lista_ul--a" href="">Vehiculos</a></li>
+                </ul>
+            </div>
+        
+    </footer>
     </div>
-   
+ 
       
     <script src="../js/sidebar.js"></script>
     <script>
